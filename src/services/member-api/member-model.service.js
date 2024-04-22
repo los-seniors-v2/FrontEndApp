887d59c42f90c888f479606c935dc47da0d4df48
Juan Diego Cueto Dominguez <u202012207@upc.edu.pe>
2024-04-22 11:32:41 -0500

    feat: added member-api service and model.

---
+++ src/services/member-api/member-model.service.js
@@ -0,0 +1,9 @@
+export class Member {
+    constructor(id, name, email, photo, subscription) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.photo = photo;
+        this.subscription = subscription;
+    }
+}
