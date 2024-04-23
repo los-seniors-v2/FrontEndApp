a410c0e439d28be3598ef5071579dddcd051cda3
JuanPescoran <u20221c936@upc.edu.pe>
2024-04-23 16:00:16 -0500

    feat(routine-card): imported template from vue documentation to work with

--- src/routines/components/routine-card.component.vue
+++ src/routines/components/routine-card.component.vue
@@ -1,13 +1,35 @@
 <script>
+import {Routine} from "../model/routine.entity.js";
+
 export default {
-  name: "routine-card.component"
+  name: "routine-card",
+  props:{
+    routine: {
+      type: Routine,
+      required: true
+    }
+  }
 }
 </script>
 
 <template>
-
+  <pv-card class="m-2">
+    <template #header>
+      <img alt="Imagen de Rutina" src="" />
+    </template>
+    <template #title>Advanced Card</template>
+    <template #subtitle>Card subtitle</template>
+    <template #content>
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+        quas!
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex gap-3 mt-1">
+        <Button label="Cancel" severity="secondary" outlined class="w-full" />
+        <Button label="Save" class="w-full" />
+      </div>
+    </template>
+  </pv-card>
 </template>
-
-<style scoped>
-
-</style>
