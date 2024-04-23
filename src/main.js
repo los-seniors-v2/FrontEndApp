936b13a6fab5d2c3d9371b472ee384559aeaa1f2
Fabrizio0711 <u202213652@upc.edu.pe>
2024-04-23 10:15:38 -0500

    feat: added membership branch

--- src/main.js
+++ src/main.js
@@ -2,4 +2,39 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import PrimeVue from "primevue/config";
+
+import 'primevue/resources/themes/aura-light-cyan/theme.css';
+import 'primeicons/primeicons.css';
+
+
+// Add UI Components for application
+import Button       from "primevue/button";
+import Card         from "primevue/card";
+import SelectButton from "primevue/selectbutton";
+import Sidebar      from "primevue/sidebar";
+import Avatar       from "primevue/avatar";
+import Menu         from "primevue/menu";
+import Menubar      from "primevue/menubar";
+import Toolbar      from "primevue/toolbar";
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+
+const app = createApp(App);
+
+// Add configuration for PrimeVue Plugin and components
+app.use(PrimeVue, { ripple: true })
+    .component('Button', Button)
+    .component('Card', Card)
+    .component('pv-select-button', SelectButton)
+    .component('Sidebar', Sidebar)
+    .component('Avatar', Avatar)
+    .component('Menu', Menu)
+    .component('Menubar', Menubar)
+    .component('Toolbar', Toolbar)
+    .component('Accordion', Accordion)
+    .component('AccordionTab', AccordionTab);
+// Mount the app
+app.mount('#app');
+
