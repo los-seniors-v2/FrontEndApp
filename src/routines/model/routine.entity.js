a9e464896d5e3de48de1bcd249b6f4c19090a71b
JuanPescoran <u20221c936@upc.edu.pe>
2024-04-23 16:01:13 -0500

    refactor(routine.entity): fixed value

--- src/routines/model/routine.entity.js
+++ src/routines/model/routine.entity.js
@@ -1,6 +1,6 @@
 export class Routine {
     constructor(id, name, instructions, exercises, coach, user) {
-        this.id = id3
+        this.id = id
         this.title = name;
         this.instructions = instructions;
         this.exercises = exercises;
