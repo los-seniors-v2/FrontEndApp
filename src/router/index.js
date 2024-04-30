4861706140b85a2cd9eb6e4c0b93c2a57f8df700
JuanPescoran <u20221c936@upc.edu.pe>
2024-04-30 14:20:35 -0500

    feat(main): added new path

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
-import RoutineList from "../routines/components/routine-list.component.vue"
-import Plans from "../components/plans-section.component.vue"
-import BookASessionComponent from "../components/book-a-session.component.vue";
-import HomeSection from "../components/home-section.vue";
+import RoutineList from "../app/routines/components/routine-list.component.vue"
+import Plans from "../app/components/plans-section.component.vue"
+import BookASessionComponent from "../app/components/book-a-session.component.vue";
+import HomeSection from "../app/components/home-section.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,7 +10,8 @@ const router = createRouter({
         { path: '/routine-list',component: RoutineList},
         { path: '/plans',component: Plans},
         { path: '/programming-session',component: BookASessionComponent},
-        { path: '/home',component: HomeSection}
+        { path: '/home',component: HomeSection},
+        { path: '/',        redirect: '/home'}
     ]
 });
 
