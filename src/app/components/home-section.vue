55d4b9b29a83b87ab30a0689edb1b09d3df2006a
JuanPescoran <u20221c936@upc.edu.pe>
2024-04-30 14:22:35 -0500

    feat(components): added home section

---
+++ src/app/components/home-section.vue
@@ -0,0 +1,43 @@
+<script>
+import FoodItemList from '../food-item/components/card-food-list.component.vue'
+import CalenderComponent from '../calenders/components/calender.component.vue'
+export default {
+  name: "home-section",
+  components: {
+    FoodItemList,
+    CalenderComponent
+  },
+}
+</script>
+
+<template>
+  <div class="banner">
+    <div class="content">
+      <br>
+      <h1>Welcome to Your Gym App!</h1><br>
+      <p>Track your workout plans and nutrition here.</p>
+      <br>
+    </div>
+
+  </div>
+
+  <FoodItemList/>
+  <div class="coach-programming">
+    <p>Do you want to schedule a session with our coaches?</p>
+    <CalenderComponent/>
+  </div>
+</template>
+
+<style scoped>
+.banner {
+  align-items: center;
+  background-image: url('../../assets/images/banner-flexpal.png');
+}
+.coach-programming{
+  text-align: center;
+}
+.content {
+  text-align: center;
+  color: #17A9C9;
+}
+</style>
