d0de063e7be92c2dcf2b48e277e9985bbdbb1223
JuanPescoran <u20221c936@upc.edu.pe>
2024-04-30 02:37:07 -0500

    feat(app): added code to work with routes
    also renamed to keep conventions

---
+++ src/app.vue
@@ -0,0 +1,63 @@
+<script>
+
+  /*import ToolbarSessionComponent from "./components/Training-session/toolbar-session.component.vue";
+  import QuestionSection from "./components/Training-session/question-section.component.vue";
+  import DetailsSection from "./components/Training-session/details-section.component.vue";
+  import TrainerSection from "./components/Training-session/trainer-section.component.vue";
+  import GuidelinesSection from "./components/Training-session/guidelines-section.component.vue";
+
+  export default {
+      components: {
+      ToolbarSessionComponent, QuestionSection, DetailsSection, GuidelinesSection, TrainerSection
+    }
+  };*/
+  export default {
+    name: 'app',
+    title: 'Flex Pal',
+    data() {
+      return {
+        drawer: false,
+        items: [
+          { label: 'Routine List', to: '/routine-list'},
+          { label: 'Plans', to: '/plans'},
+          { label: 'Programming Session', to: '/programming-session'},
+          { label: 'Home', to: '/home'},
+        ]
+      }
+    },
+    methods: {
+      toggleDrawer() {
+        this.drawer = !this.drawer
+      }
+    }
+  }
+</script>
+
+<template>
+  <Toast/>
+    <header>
+      <Toolbar class="bg-primary" fixed>
+        <template #start>
+          <a href="/home">
+          <img src="/src/assets/flexpal-logo.png" height="60px" width="250px">
+          </a>
+        </template>
+        <template #end>
+          <div class="flex-column">
+            <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
+              <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
+            </router-link>
+          </div>
+        </template>
+      </Toolbar>
+    </header>
+  <main>
+    <router-view/>
+  </main>
+</template>
+
+
+<style >
+
+
+</style>
