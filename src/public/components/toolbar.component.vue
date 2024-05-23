9b50882e559360983b2579e0e5d77ea8f7626dd1
Fabrizio0711 <u202213652@upc.edu.pe>
2024-05-22 19:26:27 -0500

    feat: added toolbar in profiles component

--- src/public/components/toolbar.component.vue
+++ src/public/components/toolbar.component.vue
@@ -1,37 +1,52 @@
 <script>
+
 export default {
-  name: "toolbar-component"
+  name: "toolbar-component",
+
 }
 </script>
 
 <template>
 
-  <div>
-
   <pv-toolbar class="toolbar">
     <template #start>
-        <img src="../../assets/images/flexpal-logo.png" alt="Flex Pal Logo" style="width: 400px; height: 100px;"/>
+        <img src="../../assets/images/flexpal-logo.png" alt="Flex Pal Logo" style="width: 350px; height: 70px;"/>
     </template>
 
-    <template #center>
-    </template>
-
-    <template #end>
-      <ul class="list-none p-0 m-0 overflow-hidden" style="display:flex; ">
-        <li>
-         <a href="/home"> Home </a>
-        </li>
-        <li>
-          <a href="/routines"> Routines </a>
-        </li>
-      </ul>
+    <template #end >
+      <div class="input">
+      <pv-InputText type="text" v-model="value" class="inner_input" placeholder="Search in site"/>
+      <span class="pi pi-search" style="font-size: 1.5rem"></span>
+      </div>
     </template>
   </pv-toolbar>
-
-  </div>
 
 </template>
 
 <style scoped>
+.toolbar{
+  width: 100vw;
+  display: flex;
+  justify-content: space-around;
 
+  li{
+    list-style-type: none;
+    display: inline;
+    padding: 10px;
+  }
+  .input{
+    border:2px solid gray;
+    border-radius: 10px;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    .inner_input{
+      border:none;
+      outline: none;
+      margin-right: 5px;
+      background-color: transparent;
+    }
+  }
+
+}
 </style>
